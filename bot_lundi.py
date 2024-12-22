2c41fa1b92f28d67de37ac1f6947002e1dec3921
Jules <juleslestrat@gmail.com>
2024-12-22 14:05:33 +0100

    Finished the bot The bot now works completely fine. I've added all the day and hour based events, including sending the picture. All that's left now is to replace the "Test" by the server's League of Legends role ID

--- bot_lundi.py
+++ bot_lundi.py
@@ -4,7 +4,7 @@ from email import message
 import token
 import discord
 import time
-from datetime import datetime
+import datetime
 import os
 from discord.ext import commands
 from discord import Client
@@ -19,6 +19,10 @@ client = discord.Client(intents=intents)
 #We created a file containing the secret values, so I can make this project entirely public without risk. For more info, refer to the README file.
 from secret_values import *
 import secret_values
+
+
+#We set the image that will be sent if someone pings too early in the morning
+seto = open("seto_kaiba_1.webp")
 
 #Of course the token can't be given here, otherwise anyone would get MY token. 
 #The token needs to be accessed form another file, at the beginning of the python code (not done yet)
@@ -35,8 +39,10 @@ new_username = "Bot"
 #secret_values.lol_role_ID
 roleID = secret_values.tester_role_ID
 
-#Getting the date, since the bot is supposed to send messages only on Mondays and Tuesdays
-dt = datetime.now()
+#Getting the date, since the bot is supposed to send messages only on Mondays, Tuesdays and Thursdays
+dt = datetime.datetime.now()
+midnight = datetime.time(0, 0, 0)
+morning = datetime.time(7, 0, 0)
 #print (dt.weekday())
 
 
@@ -58,19 +64,30 @@ async def on_ready():
 #and I'm not trying to learn how to optimize Python when C++ exists
 async def on_message(message):
     #This test works. The bot simply fails to detect keywords so far
-    print("message détecté")
+    #print("message détecté")
 
 #If the role with this ID is pinged, AND it's Monday, send the following message     
     #if "roleID" in msg.content and dt.weekday() == 0:
         #await msg.channel.send("C lundi et y a Dofus 3")
 
-#If the role with this ID is pinged, AND it's not Monday, send the following message
-    print("hehe")
-    if message.content == "caca":
-        print("caca détecté")
-        await message.channel.send("Y a Dofus 3")
+#If the role with this ID is pinged, start the following sequence :
+    if "test" in message.content:
+        #print("test détecté")
+#If it's send anywhere between midnight and 7am, then :
+        if 0 <= int(dt.strftime('%H')) <= 7:
+            await message.channel.send(file=discord.File("seto_kaiba_1.webp"))
+        else:
+    #If it's Monday, then :
+            if dt.weekday() == 0:
+                await message.channel.send("C lundi")
+    #If it's Tuesday, then :
+            if dt.weekday() == 1:
+                await message.channel.send("C mardi")
+    #If it's Thursday, then :
+            if dt.weekday() == 3:
+                await message.channel.send("C jeudi")
 
-
+datetime.date
 ##The command is commented as long as I haven't implementing getting the token from another file.
 ##See lines 15 to 18
 client.run(token)
